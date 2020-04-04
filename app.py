02f6c9f1e0d097f193a9ea721e939e59bd40ef7e
manangupta12 <manangupta852@gmail.com>
2020-04-04 20:11:41 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,35 +1,65 @@
-from flask import Flask, render_template,request, redirect, url_for,jsonify
+from flask import Flask,flash, render_template,request, redirect, url_for,jsonify
 import cv2
-import sys
-import numpy
-import os
-import json
+import AddUser as db
 
-#import gesture
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 @app.route('/')
-def landing(name=None):
-    return render_template('landing.html',name=name)
+def landing():
+    return render_template('login.html')
 
-@app.route('/vote')
-def parse(name=None):
-    import face_recognize
-    print("done")
-    return render_template('index.html',name=name)
+@app.route("/login",methods=["POST"])
+def login():
+    [name,id,password] = [request.form['name'],request.form['id'],request.form['password']]
+    status = db.VerifyUser(id,password)
+    if(status!=1):
+        return render_template("Error.html",status=status)
+    if(request.form["id"]=="0000"):
+        return render_template("admin.html",name="Admin")
+    else:
+        return redirect("/vote/"+request.form['id']+"/"+request.form['name'])
 
-# @app.route('/speech')
-# def spech():
-#     control = speech.call()
-#     print(control)
-#     with open('control.json', 'w') as fp:
-#         json.dump(control,fp)
-#     return jsonify(control)
-#     #return render_template('blind.html')
+@app.route("/admin")
+def admin():
+    return render_template("admin.html",name="Admin")
+    
+@app.route('/vote/<id>/<name>',methods=["GET"])
+def vote(id,name):
+    return render_template('vote.html',name=name,id=id)
 
-@app.route('/api')
-def api():
-    with open('control.json','r') as fp:
-        return jsonify(json.load(fp))
+
+@app.route('/addVoter',methods=['GET','POST'])
+def addVoter(name=None):
+    if request.method == 'GET':
+        return render_template('addVoter.html')
+    if request.method == 'POST':
+        status = db.addVoter(request.form['Name'],request.form['Password'],request.form['Id'])
+        if(status!="Done"):
+            return "Voter already exists"
+        if(request.form['Name'].lower=="admin"):
+            return render_template("admin.html")
+        else:
+            return redirect('/admin')
+
+@app.route("/CastVote/<party>/<id>")
+def addVote(party,id):
+    status = db.AddVote(id,party)
+    return render_template("Error.html",status=status,link="/")
+
+@app.route("/results")
+def results():
+    result = db.result() 
+    print(result)
+    if(result=="0"):
+        return render_template("Error.html",status="All voters have not casted their votes yet",link="/admin")
+    
+    return render_template("results.html",d=result)
+
+@app.route("/clear")
+def clear():
+    status = db.clearDb()
+    return render_template("Error.html",status=status,link="/addVoter")
 
 if __name__ == '__main__':
     app.debug = True
